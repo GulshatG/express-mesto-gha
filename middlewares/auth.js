f2166569164556a4ea29a075a468588da5ca23b8
Mariam99999 <mariam_agadzhanyan@mail.ru>
2023-06-13 10:51:00 +0400

    fix env

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const Unauthorized = require('../exceptions/unauthorized');
 const { unauthorized } = require('../utils/validationMessage');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports.auth = (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
@@ -9,7 +10,9 @@ module.exports.auth = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(token, process.env.JWT_SECRET);
+    payload = jwt.verify(
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+    );
   } catch (err) {
     return next(new Unauthorized(unauthorized));
   }

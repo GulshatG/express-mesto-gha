a6ada71a93f9aa822bdf83168ac2a7a47e6b7545
Mariam99999 <mariam_agadzhanyan@mail.ru>
2023-06-28 16:16:23 +0400

    add cors

--- app.js
+++ app.js
@@ -26,7 +26,31 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  return next();
+});
 
 app.use(helmet());
 app.use(bodyParser.json());
